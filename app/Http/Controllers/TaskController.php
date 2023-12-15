90f3fd12d6b705b327dc1b34130863c319613ea2
SaraBarrett <sara.c.mon@gmail.com>
2023-12-15 20:33:48 +0000

    tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,14 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Console\View\Components\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
     public function allTasks(){
         $tasks= $this->getAllTasks();
 
-        return view('tasks.all_tasks', compact('tasks'));
+        $dbTasks = DB::table('tasks')
+                    ->join('users', 'users.id', '=', 'tasks.user_id')
+                    ->select('tasks.*', 'users.name as user_name')
+                    ->get();
+
+        //dd($dbTasks);
+
+        return view('tasks.all_tasks', compact(
+            'tasks',
+            'dbTasks'
+
+    ));
     }
 
     protected function getAllTasks(){
--- resources/views/tasks/all_tasks.blade.php
+++ resources/views/tasks/all_tasks.blade.php
@@ -2,11 +2,32 @@
 
 
 @section('content')
-    <h1>Aqui tens todas as Tarefas</h1>
+    <div class="container">
+        <h1>Aqui tens todas as Tarefas</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">User</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($dbTasks as $task)
+                    <tr>
+                        <th scope="row">{{ $task->id }}</th>
+                        <td>{{ $task->name }}</td>
+                        <td>{{ $task->user_name }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
 
-    <ul>
-        @foreach ($tasks as $item)
-            <li>{{ $item[1] }}</li>
-        @endforeach
-    </ul>
+
+        <ul>
+            @foreach ($tasks as $item)
+                <li>{{ $item[1] }}</li>
+            @endforeach
+        </ul>
+    </div>
 @endsection

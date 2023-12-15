de1f9d7f7580c62b8b55e91299eca23ef0dfd2d5
SaraBarrett <sara.c.mon@gmail.com>
2023-12-15 19:53:03 +0000

    allusers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,7 +30,6 @@ class UserController extends Controller
 
 
 
-
         /*DB::table('users')
         ->updateOrInsert(
             ['email' => 'Eloy86@gmail.com'],
--- resources/views/users/all_users.blade.php
+++ resources/views/users/all_users.blade.php
@@ -2,33 +2,58 @@
 
 
 @section('content')
-    <h2>Olá. Aqui iremos mostrar todos os utilizadores</h2>
-    Os utilizadores são:
+    <div class="container">
+        {{-- <h2>Olá. Aqui iremos mostrar todos os utilizadores</h2>
+        Os utilizadores são:
 
-    {{-- Users --}}
-    <ul>
-        <li> {{ $users[0] }} a tarefa é {{ $tasks[1] }}</li>
-        <li> {{ $users[1] }}</li>
-        <li>{{ $users[2] }} tem {{ $contactInfo['age'] }}</li>
-    </ul>
-
-
-    {{-- Contactos --}}
-    <ul>
-        @foreach ($contacts as $contact)
-            <li>{{ $contact[1] }} e o telefone é {{ $contact[2] }}</li>
-        @endforeach
-    </ul>
+        {{-- Users --}}
+        {{-- <ul>
+            <li> {{ $users[0] }} a tarefa é {{ $tasks[1] }}</li>
+            <li> {{ $users[1] }}</li>
+            <li>{{ $users[2] }} tem {{ $contactInfo['age'] }}</li>
+        </ul> --}}
 
 
-    <h1>Testes de Mysql</h1>
-    <h4>{{-- $dbusers->name --}} - {{-- $dbusers->email --}}</h4>
+        {{-- Contactos --}}
+        {{-- <ul>
+            @foreach ($contacts as $contact)
+                <li>{{ $contact[1] }} e o telefone é {{ $contact[2] }}</li>
+            @endforeach
+        </ul> --}}
 
-    <h4>Todos os Users</h4>
-    <ul>
-        @foreach ($allDbUsers as $item)
-            <li>{{ $item->name }} e o email é {{ $item->email }}</li>
-        @endforeach
 
-    </ul>
+        {{-- <h1>Testes de Mysql</h1>
+        <h4> $dbusers->name - $dbusers->email </h4>
+
+        <h4>Todos os Users</h4>
+        <ul>
+            @foreach ($allDbUsers as $item)
+                <li>{{ $item->name }} e o email é {{ $item->email }}</li>
+            @endforeach
+        </ul> - --}}
+
+        <h4>Todos os Users</h4>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col">Password</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($allDbUsers as $item)
+                    <tr>
+                        <th scope="row">{{ $item->id }}</th>
+                        <td>{{ $item->name }}</td>
+                        <td>{{ $item->email }}</td>
+                        <td>{{ $item->phone }}</td>
+                        <td>{{ $item->password }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
